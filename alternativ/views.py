f92c42fb5505d3e933b55a353c2b776153fe7b4a
fromtheothershore <kirien.chen@gmail.com>
2014-03-08 10:21:02 -0500

    in prog stuff

--- alternativ/views.py
+++ alternativ/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render
+from alternativ.models import Item
 # VIEWS: return an HttpResponse... or raise Http404 if invalid request.
 
 def index(request):
@@ -18,7 +19,7 @@ def index(request):
     return HttpResponse(template.render(context))
 
 def detail(request, item_type):
-	#template = loader.get_template('type.html')
+	template = loader.get_template('type.html')
 	# get ikea item
 	ikea_item = Item.objects.filter(store_name="IKEA", item_type__iexact="%s" % item_type)
 	# get 4 alternative items
@@ -27,8 +28,8 @@ def detail(request, item_type):
 		'ikea_item': ikea_item,
 		'alt_items': alt
 		})
-	#return HttpResponse(template.render(context))
-	return HttpResponse("You're looking at category: %s." % item_type)
+	return HttpResponse(template.render(context))
+	#return HttpResponse("You're looking at category: %s." % item_type)
 
 def options(request, item_type, item_name):
     return HttpResponse("Category: %s. ~~ Alternativ furniture item: %s" % (item_type, item_name))

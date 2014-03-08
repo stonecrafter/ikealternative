c921ffa044bf1ed838348360b9c74c034d110707
fromtheothershore <kirien.chen@gmail.com>
2014-03-07 22:52:58 -0500

    .

--- alternativ/settings.py
+++ alternativ/settings.py
@@ -76,6 +76,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -111,9 +112,7 @@ ROOT_URLCONF = 'alternativ.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'alternativ.wsgi.application'
 
-PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
-
-STATIC_URL = '/static/'
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".

05807963b584e3a82cac99644449f56cfe664ace
fromtheothershore <kirien.chen@gmail.com>
2014-03-15 16:41:34 -0400

    some imagessssss

--- alternativ/views.py
+++ alternativ/views.py
@@ -4,26 +4,39 @@ from django.shortcuts import render
 from alternativ.models import Item, Store
 # VIEWS: return an HttpResponse... or raise Http404 if invalid request.
 
+# Globals
+TYPES = {'ARM CHAIR': '01', 
+		  'COUCH': '02', 
+		  'COFFEE TABLE': '03', 
+		  'DESK': '04', 
+		  'BED': '05',
+		  'DINING TABLE': '06', 
+		  'DINING CHAIR': '07', 
+		  'SHELVING': '08'
+}
+
+STORES = {'CB2': '01',
+		   'EQ3': '02',
+		   'GAUTIER': '03',
+		   'THE BAY': '04',
+		   'WEST ELM': '05',
+		   'WESTELM': '05',  # kinda hacky, it's OK for now
+		   'BO CONCEPT': '06',   # in the interest of deadlines :/
+		   'BOCONCEPT': '06'
+}
+
 
 def about(request):
-    template = loader.get_template('about.html')
-    context = RequestContext(request, {})
-    return HttpResponse(template.render(context))
+	template = loader.get_template('about.html')
+	context = RequestContext(request, {})
+	return HttpResponse(template.render(context))
 
 
 def index(request):
-    template = loader.get_template('index.html')
-    context = RequestContext(request, {
-        'types': {'ARM CHAIR': '01', 
-        		  'COUCH': '02', 
-        		  'COFFEE TABLE': '03', 
-        		  'DESK': '04', 
-        		  'BED': '05',
-        		  'DINING TABLE': '06', 
-        		  'DINING CHAIR': '07', 
-        		  'SHELVING': '08'},
-    })
-    return HttpResponse(template.render(context))
+	template = loader.get_template('index.html')
+	context = RequestContext(request, {
+		'types': TYPES})
+	return HttpResponse(template.render(context))
 
 
 def detail(request, item):
@@ -32,40 +45,46 @@ def detail(request, item):
 	ikea_item = Item.objects.filter(store_name="IKEA", item_type__iexact="%s" % Item.TYPE_CHOICES[int(item) - 1][1])[0]
 	# get all the alternative items
 	alt = Item.objects.exclude(store_name="IKEA").filter(item_type__iexact="%s" % Item.TYPE_CHOICES[int(item) - 1][1])
+	# create mapping between store name and code
+	item_list = {}
+	for i in alt:
+		item_list[i] = STORES[str(i.store_name.store_name).upper()]
+	# pass in the context
 	context = RequestContext(request, {
-        'item_type': Item.TYPE_CHOICES[int(item) - 1][1],
+		'item_type': Item.TYPE_CHOICES[int(item) - 1][1],
 		'ikea_item': ikea_item.description,
-        'subj': ikea_item.subjective_input,
-		'alt_items': alt,
-        'code': item
+		'subj': ikea_item.subjective_input,
+		'alt_items': item_list,
+		'code': item
 		})
 	return HttpResponse(template.render(context))
 
 
 def options(request, item_type, item):
-    # item_type can be used for a back button if desired
-    template = loader.get_template('option.html')
-    # get the item
-    item = Item.objects.get(id=item)
-    store = Store.objects.get(store_name=item.store_name)
-    full_address = store.store_address + " " + store.city + " " + store.province + " " + store.postal_code
-    context = RequestContext(request, {
-        # item specific
-        'item_code': item,
-        'type_code': item_type,
-        'item_name': item.item_name,
-        'item_desc': item.description,
-        'subj': item.subjective_input,
-        'price': item.price_multipler,
-        'store_name': item.store_name,
-        'item_link': item.item_link,
-        # store specific
-        'store_address': store.store_address,
-        'store_website': store.store_website,
-        'city': store.city,
-        'province': store.province,
-        'postal_code': store.postal_code,
-        'phone': store.phone_number,
-        'full_address': full_address
-        })
-    return HttpResponse(template.render(context))
+	# item_type can be used for a back button if desired
+	template = loader.get_template('option.html')
+	# get the item
+	item = Item.objects.get(id=item)
+	store = Store.objects.get(store_name=item.store_name)
+	full_address = store.store_address + " " + store.city + " " + store.province + " " + store.postal_code
+	context = RequestContext(request, {
+		# item specific
+		'item_code': item,
+		'type_code': item_type,
+		'item_name': item.item_name,
+		'item_desc': item.description,
+		'subj': item.subjective_input,
+		'price': item.price_multipler,
+		'store_name': item.store_name,
+		'item_link': item.item_link,
+		# store specific
+		'store_code': STORES[str(item.store_name).upper()],
+		'store_address': store.store_address,
+		'store_website': store.store_website,
+		'city': store.city,
+		'province': store.province,
+		'postal_code': store.postal_code,
+		'phone': store.phone_number,
+		'full_address': full_address
+		})
+	return HttpResponse(template.render(context))

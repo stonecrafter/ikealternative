251484086182e458470ee06aad8010ab57a7b2e2
fromtheothershore <kirien.chen@gmail.com>
2014-03-22 16:38:10 -0400

    tedjango debug settings suck

--- alternativ/settings.py
+++ alternativ/settings.py
@@ -8,8 +8,6 @@ mimetypes.add_type("image/svg+xml", ".svg", True)
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ["alternativ.herokuapp.com"]
-
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -30,7 +28,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alternativ.herokuapp.com', '127.0.0.1']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name

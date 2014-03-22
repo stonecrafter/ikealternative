872c743eba00ce8d85d7066305558aed5812f2f1
fromtheothershore <kirien.chen@gmail.com>
2014-03-22 16:40:44 -0400

    deployment successful, reverting to dev settings

--- alternativ/wsgi.py
+++ alternativ/wsgi.py
@@ -27,10 +27,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alternativ.settings")
 from django.core.wsgi import get_wsgi_application
 
 ## THESE TWO LINES ARE FOR DEPLOYMENT ONLY ##
-from dj_static import Cling
-application = Cling(get_wsgi_application())
+#from dj_static import Cling
+#application = Cling(get_wsgi_application())
 
-#application = get_wsgi_application()
+application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
